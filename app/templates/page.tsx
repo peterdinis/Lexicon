62c0379c34636b06158014c84205a1a03b09cc05
peterdinis <petter1@pobox.sk>
2025-08-09 19:37:23 +0200

    feat: Start working on new pages WIP

---
+++ app/templates/page.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+
+const TemplatesPage: NextPage = () => {
+    return (
+        <></>
+    )
+}
+
+export default TemplatesPage
---
+++ app/trash/page.tsx
@@ -0,0 +1,8 @@
+import TrashWrapper from "@/components/trash/TrashWrapper";
+import { NextPage } from "next";
+
+const TrashPage: NextPage = () => {
+    return <TrashWrapper />
+}
+
+export default TrashPage
