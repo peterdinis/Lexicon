349c753b048bba98c00a676f15751ebc35ad2b58
peterdinis <petter1@pobox.sk>
2025-09-06 17:59:59 +0200

    feat: Start preparing logic for uploading custom files

---
+++ app/files/page.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+
+const MyFilesPage: NextPage = () => {
+    return (
+        <>ABCD</>
+    )
+}
+
+export default MyFilesPage
---
+++ components/files/UploadFileDialog.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { FC, useState } from "react";
+import { Dialog, DialogContent} from "../ui/dialog";
+
+type UploadDialogProps = {
+  uploadDialogOpen: boolean;
+  setUploadDialogOpen: (uploadDialogOpen: boolean) => void;
+};
+
+const UploadDialog: FC<UploadDialogProps> = ({
+  uploadDialogOpen,
+  setUploadDialogOpen,
+}: UploadDialogProps) => {
+  const [activeTab, setActiveTab] = useState("account");
+
+  return (
+    <Dialog open={uploadDialogOpen} onOpenChange={setUploadDialogOpen}>
+      <DialogContent className="max-w-4xl max-h-[80vh] overflow-hidden p-0">
+        TODO
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default UploadDialog;
