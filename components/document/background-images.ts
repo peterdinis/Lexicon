547b2554015697285ab97aa3cfbbf63d8004e772
peterdinis <petter1@pobox.sk>
2025-09-24 12:29:00 +0200

    feat: New background-images and colors

--- components/document/background-images.ts
+++ components/document/background-images.ts
@@ -5,4 +5,10 @@ export const backgroundImages = [
   "https://images.unsplash.com/photo-1518837695005-2083093ee35b?w=800&q=80",
   "https://images.unsplash.com/photo-1497436072909-f5e92c2b7b20?w=800&q=80",
   "https://images.unsplash.com/photo-1506905925346-21bda4d32df4?w=800&q=80",
+  "https://images.unsplash.com/photo-1516225357-2d6e0c7f9d7b?w=800&q=80",
+  "https://images.unsplash.com/photo-1506748686215-1f4f0a0b2d8f?w=800&q=80",
+  "https://images.unsplash.com/photo-1518756131211-1e1e5d6c1e6e?w=800&q=80",
+  "https://images.unsplash.com/photo-1495566580170-3e5e2e5c9a9e?w=800&q=80",
+  "https://images.unsplash.com/photo-1506748686215-1f4f0a0b2d8f?w=800&q=80",
+  "https://images.unsplash.com/photo-1516225357-2d6e0c7f9d7b?w=800&q=80",
 ];
