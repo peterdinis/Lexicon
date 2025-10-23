a37510497e9cea0823bb3c3c9802f0706bd3128f
peterdinis <petter1@pobox.sk>
2025-10-23 16:16:09 +0200

    infra: Remove settings page

---
+++ app/trash/loading.tsx
@@ -0,0 +1,8 @@
+import { Spinner } from "@/components/ui/spinner"
+import { FC, Suspense } from "react"
+
+const Loading: FC = () => {
+    return <Suspense fallback={<Spinner />} />
+}
+
+export default Loading
