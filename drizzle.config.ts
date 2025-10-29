4f98d568bcaee561b7977622c7029452415d0eff
peterdinis <petter1@pobox.sk>
2025-10-29 10:03:27 +0100

    infra: Start to migrate to postgresql WIP

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,7 +4,7 @@ import { Config, defineConfig } from "drizzle-kit";
 export default defineConfig({
   out: "./drizzle",
   schema: "./drizzle/schema.ts",
-  dialect: "sqlite",
+  dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
