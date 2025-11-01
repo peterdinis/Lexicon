180cbbc8dc33610b4e22ebb210918f5c9aaef5f0
peterdinis <petter1@pobox.sk>
2025-11-01 19:17:47 +0100

    fix: Fixing bug with page id

--- app/page/[id]/page.tsx
+++ app/page/[id]/page.tsx
@@ -2,17 +2,14 @@ import { redirect } from "next/navigation";
 import PageViewClient from "@/components/pages/PageViewClient";
 import { getPageHandler, getAllPagesHandler } from "@/actions/pagesActions";
 
-// Povoliť dynamický rendering
-export const dynamic = "force-dynamic";
-
 interface PageViewProps {
-  params: {
+  params: Promise<{
     id: string;
-  };
+  }>;
 }
 
 export default async function PageView({ params }: PageViewProps) {
-  const { id } = params;
+  const { id } = await params;
 
   if (!id) {
     console.error("Page ID is undefined");
@@ -31,6 +28,7 @@ export default async function PageView({ params }: PageViewProps) {
 
     return <PageViewClient id={id} page={page} pages={pages} />;
   } catch (error) {
-    throw new Error("Something went wrong");
+    console.error("Error loading page:", error);
+    redirect("/dashboard");
   }
 }
